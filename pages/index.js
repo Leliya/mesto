08e9748c04e98c55a1023c2d8eef6d45fe80fadf
Leliya <98910653+Leliya@users.noreply.github.com>
2022-04-12 22:53:03 +0400

    feature: реализовать публикацию полей в profile

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,34 @@
 const popup = document.querySelector(".popup");
 const edit = document.querySelector(".profile__edit-button");
+const popupClose = popup.querySelector(".popup__close");
+const formElement = popup.querySelector(".popup__container");
+const nameInput = formElement.querySelector(".popup__field-name");
+const activityInput = formElement.querySelector(".popup__field-activity");
+const nameProfile = document.querySelector(".profile__name");
+const activityProfile = document.querySelector(".profile__activity");
+const save = popup.querySelector(".popup__button");
 
 edit.addEventListener("click", function () {
   popup.classList.add("popup_opened");
 });
 
-const popupClose = popup.querySelector(".popup__close");
-
 popupClose.addEventListener("click", function () {
   popup.classList.remove("popup_opened");
 });
 
-console.log(popup.classList.contains("popup_opened"));
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  const nameInput = formElement.querySelector(".popup__field-name");
+  const activityInput = formElement.querySelector(".popup__field-activity");
+  const nameProfile = document.querySelector(".profile__name");
+  const activityProfile = document.querySelector(".profile__activity");
+
+  save.addEventListener("click", function () {
+    nameProfile.textContent = nameInput.value;
+    activityProfile.textContent = activityInput.value;
+    popup.classList.remove("popup_opened");
+  });
+}
+
+formElement.addEventListener("submit", formSubmitHandler);
