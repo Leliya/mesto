0a577118bcf4b87ca9e7aed2561a1ea2aac361e4
Leliya <98910653+Leliya@users.noreply.github.com>
2022-04-18 00:29:02 +0400

    Устранены замечания ревьюера

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,6 @@ const popup = document.querySelector(".popup");
 const edit = document.querySelector(".profile__edit-button");
 const popupClose = popup.querySelector(".popup__close");
 const formElement = popup.querySelector(".popup__container");
-const nameInput = formElement.querySelector(".popup__field-name");
-const activityInput = formElement.querySelector(".popup__field-activity");
-const nameProfile = document.querySelector(".profile__name");
-const activityProfile = document.querySelector(".profile__activity");
 const save = popup.querySelector(".popup__button");
 
 edit.addEventListener("click", function () {
@@ -19,16 +15,16 @@ popupClose.addEventListener("click", function () {
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
-  const nameInput = formElement.querySelector(".popup__field-name");
-  const activityInput = formElement.querySelector(".popup__field-activity");
+  const nameInput = formElement.querySelector(".popup__input_type_name");
+  const activityInput = formElement.querySelector(
+    ".popup__input_type_activity"
+  );
   const nameProfile = document.querySelector(".profile__name");
   const activityProfile = document.querySelector(".profile__activity");
 
-  save.addEventListener("click", function () {
-    nameProfile.textContent = nameInput.value;
-    activityProfile.textContent = activityInput.value;
-    popup.classList.remove("popup_opened");
-  });
+  nameProfile.textContent = nameInput.value;
+  activityProfile.textContent = activityInput.value;
+  popup.classList.remove("popup_opened");
 }
 
 formElement.addEventListener("submit", formSubmitHandler);
