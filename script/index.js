822ad04948607fe749e5809074ce01b807bcfc9c
Leliya <98910653+Leliya@users.noreply.github.com>
2022-04-19 13:48:38 +0400

    hotfix: исправить переполнение в profile

--- script/index.js
+++ script/index.js
@@ -2,16 +2,15 @@ const popup = document.querySelector('.popup');
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = popup.querySelector('.popup__close-button');
 const formElement = popup.querySelector('.popup__container');
-const popupSaveButton = popup.querySelector('.popup__button');
 const nameInput = formElement.querySelector('.popup__input_type_name');
 const activityInput = formElement.querySelector('.popup__input_type_activity');
 const nameProfile = document.querySelector('.profile__name');
 const activityProfile = document.querySelector('.profile__activity');
 
 function openPopup() {
-  popup.classList.add('popup_opened');
   nameInput.value = nameProfile.textContent;
   activityInput.value = activityProfile.textContent;
+  popup.classList.add('popup_opened');
 }
 
 function closePopup() {
